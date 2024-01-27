beeec1ca59a80f8d4d58c628e66a26e30c660247
ichiri0 <quarkkkk@email.com>
2024-01-27 13:27:58 +0300

    обновлены настройки проекта

--- TestTaskMirGovorit/settings.py
+++ TestTaskMirGovorit/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +56,8 @@ ROOT_URLCONF = 'TestTaskMirGovorit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # Указываем правильный путь
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,7 +69,6 @@ TEMPLATES = [
         },
     },
 ]
-
 WSGI_APPLICATION = 'TestTaskMirGovorit.wsgi.application'
 
 
